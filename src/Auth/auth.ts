894a60b5846795f084cf1da091a83570a7931a81
Shawn Bellazan <tooensuremaster@gmail.com>
2022-02-18 03:43:39 -0600

    bug fix for user recod import

--- src/Auth/auth.ts
+++ src/Auth/auth.ts
@@ -1,15 +1,11 @@
 import {IAuth} from "./IAuth/i-auth";
 import * as admin from "firebase-admin";
-import {UserRecord} from "firebase-functions/v1/auth";
 import {RegisterAuth} from "./register.auth";
+import { UserRecord } from "firebase-admin/lib/auth/user-record";
 
 export class Auth extends RegisterAuth implements IAuth {
   async getUidByToken(token: string): Promise<UserRecord> {
-    try {
-      const decodedJwt = await admin.auth().verifyIdToken(token);
-      return await admin.auth().getUser(decodedJwt.uid);
-    } catch (error) {
-      return error;
-    }
+    const decodedJwt = await admin.auth().verifyIdToken(token);
+    return await admin.auth().getUser(decodedJwt.uid);
   }
 }
--- src/Auth/register.auth.ts
+++ src/Auth/register.auth.ts
@@ -1,22 +1,15 @@
 import * as admin from "firebase-admin";
-import {UserRecord} from "firebase-functions/v1/auth";
+import { UserRecord } from "firebase-admin/lib/auth/user-record";
 import {IRegisterAuth} from "./IAuth/i-register.auth";
 
 export class RegisterAuth implements IRegisterAuth {
   async signUpWithEmailPassword(
-      email: string, password: string): Promise<UserRecord> {
-    try {
-      console.log("Registering user...");
-      const registeredUser = await admin.auth().createUser({
+    email: string, password: string): Promise<UserRecord> {
+      return await admin.auth().createUser({
         displayName: email,
         email: email,
         emailVerified: true,
         password: password,
-      });
-
-      return registeredUser;
-    } catch (error) {
-      return error;
-    }
+    });
   }
 }
